e639ae798755ebea88ae7007a20ea10e90010ce6
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-09 20:19:51 +0800

    debug: add initial email and password default values for testing purposes

--- lib/screens/auth/login_page.dart
+++ lib/screens/auth/login_page.dart
@@ -18,8 +18,8 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   // [CONTROLLERS]
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController(text: "adriandominic.tan@wvsu.edu.ph");
+  final TextEditingController _passwordController = TextEditingController(text: "123456");
 
   // [STATES]
   bool _isLoading = false;
