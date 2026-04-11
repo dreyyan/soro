bf21076b6bd20d699998df60317f77522b64fca6
dreyyan <adriandominic.tan@wvsu.edu.ph>
2026-04-11 18:49:11 +0800

    style(header): add mascot head to header widget

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -11,23 +11,59 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
-      height: 84,
-      alignment: Alignment.center,
-      decoration: const BoxDecoration(
-        color: AppColors.primary_500,
-        borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(60),
-        ),
-      ),
-      child: Text(
-        title,
-        style: const TextStyle(
-          color: AppColors.secondary_300,
-          fontFamily: "TheFoxTail",
-          fontSize: 62,
-        ),
+      child: Column(
+        children: [
+          // [BAR] Top
+          Container(
+            height: 32,
+            width: double.infinity,
+            color: AppColors.primary_700,
+          ),
+
+          // [BAR] Middle
+          Container(
+            height: 32,
+            width: double.infinity,
+            color: AppColors.primary_600,
+          ),
+
+          // [BAR] Bottom with Title
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              color: AppColors.primary_500,
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(40),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: AppColors.secondary_50,
+                    fontFamily: "TheFoxTail",
+                    fontSize: 64,
+                  ),
+                ),
+                // [MASCOT IMAGE]
+                Padding(
+                  padding: const EdgeInsets.only(left: 16),
+                  child: Image.asset(
+                    'assets/images/soro-mascot-head.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
